def9bbcbd0ed585f19b3a616883cda0c90dd7460
Bais <nilanbais@outlook.com>
2024-05-07 17:42:35 +0200

    small cleanup

--- public/js/script.js
+++ public/js/script.js
@@ -8,9 +8,17 @@ async function display_weather_report() {
 
     const weather_report = await fetch(`http://localhost:3000/weather_report/${location}`);
     const weather_json = await weather_report.json();
-
+   
     document.getElementById('temp').textContent = JSON.stringify(weather_json.data.temp, null, 2);
     document.getElementById('wind').textContent = JSON.stringify(weather_json.data.wind_speed, null, 2);
     document.getElementById('humidity').textContent = JSON.stringify(weather_json.data.humidity, null, 2);
-    document.getElementById('rad').textContent = JSON.stringify(weather_json.data.radiation_temp, null, 2);
+    document.getElementById('radiation').textContent = JSON.stringify(weather_json.data.radiation_temp, null, 2);
 };
+
+document.getElementById('submit-location').addEventListener('click', (e) => {
+    e.preventDefault();
+    let user_loc = document.getElementById('input-location').value;
+    set_weather_report(user_loc)
+});
+
+display_weather_report();
